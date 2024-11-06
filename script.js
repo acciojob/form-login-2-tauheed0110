e13804d6f9f7beffd11d12f8fbc4b91986d9d2d3
tauheed0110 <tauheed0110@gmail.com>
2024-11-06 08:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const fname = document.getElementById("First Name");
+const lname = document.getElementById("Last Name");
+const phone = document.getElementById("Phone Number");
+const email = document.getElementById("Email id");
+
+const data = {
+    fname: "",
+    lname: "",
+    phone: "",
+    email: ""
+}
+
+function showDataAlert(){
+    data.fname = fname.value;
+    data.lname = lname.value;
+    data.phone = phone.value;
+    data.email = email.value;
+
+    alert(`First Name: ${data.fname} Last Name: ${data.lname} Phone Number: ${data.phone} Email ID: ${data.email}`);
+}
+
+window.onload = ()=>{
+    document.getElementById("MyForm").onsubmit = (e)=>{
+        e.preventDefault();
+    }
+}
